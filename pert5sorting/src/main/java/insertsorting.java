cb20949b71bc13880494a9c18492b39f4d51b9c7
Hamim17asyhari <163313608+Hamim17asyhari@users.noreply.github.com>
2024-05-10 18:29:45 +0700

    Add files via upload

---
+++ pert5sorting/src/main/java/insertsorting.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author wilda
+ */
+public class insertsorting {
+     public static void main(String[] args) {
+        String[] names = {"Jeni", "Alice", "Bidin", "Evelin", "Choco", "Danang", "Fransiska"};
+        insertionSort(names);
+        System.out.println("Insertion Sort:");
+        printArray(names);
+    }
+
+    public static void insertionSort(String[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            String key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(String[] arr) {
+        for (String s : arr) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+}
